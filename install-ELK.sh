6bb643decd5714da4e0ab3798a887a3ac1284ca0
NullVibes <71892534+NullVibes@users.noreply.github.com>
2023-07-06 08:38:14 -0400

    Update install-ELK.sh

--- install-ELK.sh
+++ install-ELK.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elastic.gpg
-echo "deb [signed-by=/usr/share/keyrings/elastic.gpg] http://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
+echo "deb [signed-by=/usr/share/keyrings/elastic.gpg] http://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install elasticsearch -y
